303f5d8bdc18608d5b3837e1bb31218e2068ae9b
Thomas Parker <thomasparker@new-host-2.home>
2014-11-07 22:54:54 +0100

    First commit

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    @IBOutlet weak var dogYearsResultLabel: UILabel!
+    @IBOutlet weak var laikaStampImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYearsButton(sender: UIButton) {
+        var humanYearsInt = humanYearsTextField.text.toInt()
+        var dogYearsInt = humanYearsInt! / 7
+        laikaStampImageView.hidden = false
+        dogYearsResultLabel.text = "If you were a dog, you would be \(dogYearsInt) years old"
+        dogYearsResultLabel.hidden = false
+        humanYearsTextField.text = ""
+        self.humanYearsTextField.resignFirstResponder()
+    }
 
 }
 
